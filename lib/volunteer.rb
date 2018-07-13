c21aea5651a89bedc690cebe1d4237cd8152e05b
Eric Shull <eshulldesign@gmail.com>
2018-07-13 12:02:38 -0700

    add passing test for fourth volunteer specs. All method on Volunteer Class contains an empty array of volunteers

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -4,9 +4,30 @@ class Volunteer
   attr_accessor :name, :project_id
 
   def initialize(params)
+    params = default.merge(params)
     @name = params.fetch(:name)
     @project_id = params.fetch(:project_id)
     @id = params.fetch(:id)
+  end
+
+  def default
+    {:id=>0,:name => "none", :title => "none", :project_id => 0}
+  end
+  def self.all
+      returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+      volunteers = []
+      returned_volunteers.each() do |volunteer|
+        name = volunteer.fetch("name")
+        # volunteers = volunteer.fetch("volunteers")
+        id = volunteer.fetch("id").to_i()
+        volunteers.push(Volunteer.new({:name => name, :id => id}))
+      end
+      volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 
   def ==(volunteer)
