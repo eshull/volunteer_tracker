e7e2c64738e1c7d36f920c414315eae2868c4a75
Eric Shull <eshulldesign@gmail.com>
2018-07-13 11:17:19 -0700

    add passing test for sixth project test. Save method on object saves all new projects into database

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -45,14 +45,14 @@ describe Project do
     end
   end
 end
-#
-#   describe '#save' do
-#     it 'saves a project to the database' do
-#       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#       project.save
-#       expect(Project.all).to eq [project]
-#     end
-#   end
+
+  describe '#save' do
+    it 'saves a project to the database' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      expect(Project.all).to eq [project]
+    end
+  end
 #
 #   describe '.find' do
 #     it 'returns a project by id' do
